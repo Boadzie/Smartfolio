483998e127e3c76027e539a49a93cfafd93b121d
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-04-23 13:02:25 +0000

    Updated file forms and refactored placeholder functionality into views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,10 @@
 // about supported directives.
 //
 //= require jquery3
-//= require popper
-//= require bootstrap-sprockets 
 //= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
 //= require turbolinks
 //= require_tree .
+//= require popper
+//= require bootstrap-sprockets
