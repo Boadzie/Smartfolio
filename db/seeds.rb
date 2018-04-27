7a2c1a90e5550c2e79264bb7b44c61990b72dc7f
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-04-27 17:56:06 +0000

    Implemented custom scopes for recent blogs and hidden draft blogs from non-admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,86 +1,76 @@
 User.create!(
-  email:"daniel@gmail.com",
-  password:"incredible",
-  password_confirmation:"incredible",
+  email: "daniel@gmail.com",
+  password: "incredible",
+  password_confirmation: "incredible",
   name: "Admin User",
   roles: "site_admin"
 )
 
-puts "1 Admin was created"
+puts "1 Admin user created"
 
 User.create!(
-  email:"dan@gmail.com",
-  password:"daniel",
-  password_confirmation:"daniel",
+  email: "dan@gmail.com",
+  password: "daniel",
+  password_confirmation: "daniel",
   name: "Regular User",
-  roles: "user"
 )
 
-puts "1 regular user was created"
+puts "1 regular user created"
+
 
 3.times do |topic|
   Topic.create!(
-   title:"Topic #{topic}"
+    title: "Topic #{topic}"
   )
 end
 
-puts "3 topics created"
+puts "3 Topics created"
 
 10.times do |blog|
   Blog.create!(
-    title: "my blog post #{blog}",
-    body: "Analogies are close cousins of metaphors and also are very
-effective. An analogy is a comparison between two different
-things in order to highlight some area of similarity.",
-topic_id: Topic.last.id
- 
-  	)
+    title: "My Blog Post #{blog}",
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+    topic_id: Topic.last.id
+  )
 end
 
-puts "10 blog posts were created"
+puts "10 blog posts created"
 
 5.times do |skill|
   Skill.create!(
-     title: "Rails #{skill}",
-     percent_utilized: 50
-  	)
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
 end
 
-puts "5 skills were created"
+puts "5 skills created"
 
 8.times do |portfolio_item|
   Portfolio.create!(
-     title:"Portfolio title: #{portfolio_item}",
-     subtitle: "Ruby on Rails",
-     body: "The
-company extended the metaphor to the website with an image
-of a suit of armor and the same tagline. The metaphor was consistent throughout the company’s marketing material",
-     main_image: "https://placehold.it/600x400",
-     thumb_image: "https://placehold.it/350x200"
-  	)
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
 end
-
-puts "8 portfolio items were created"
 
 1.times do |portfolio_item|
   Portfolio.create!(
-     title:"Portfolio title: #{portfolio_item}",
-     subtitle: "Angular",
-     body: "The
-company extended the metaphor to the website with an image
-of a suit of armor and the same tagline. The metaphor was consistent throughout the company’s marketing material",
-     main_image: "https://placehold.it/600x400",
-     thumb_image: "https://placehold.it/350x200"
-    )
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
 end
 
-puts "1 portfolio items were created"
+puts "9 portfolio items created"
 
 3.times do |technology|
   Portfolio.last.technologies.create!(
-     name:"Technology #{technology}"
-    )
+    name: "Technology #{technology}"
+  )
 end
 
-puts "3 technologies were created"
-
+puts "3 technologies created"
