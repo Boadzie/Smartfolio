a1aad3be9849e4764eb1254ec27d02cf389362d3
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-04-28 22:11:34 +0000

    Built out the complete crud operations for topic controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,21 +1,72 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
   layout 'blog'
 
   def index
-    @topics = Topic.page(params[:page]).per(5)
+    @topics = Topic.recent.page(params[:page]).per(5)
   end
 
   def show
-    @topic = Topic.find(params[:id])
     if logged_in?(:site_admin)
       @blogs = @topic.blogs.recent.page(params[:page]).per(5) 
     else
-      @blogs = @topic.blogs.recent.page(params[:page]).per(5) 
+      @blogs = @topic.blogs.published.recent.page(params[:page]).per(5) 
     end
   end
 
   def new
     @topic = Topic.new
   end
+
+  def edit
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /blogs/1
+  # PATCH/PUT /blogs/1.json
+  def update
+    respond_to do |format|
+      if @topic.update(topic_params)
+        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  def destroy
+    @topic.destroy
+    respond_to do |format|
+      format.html { redirect_to topics_url, notice: 'Topic was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def topic_params
+      params.require(:topic).permit(:title)
+    end
 
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic < ApplicationRecord
 	validates_presence_of :title
 	has_many :blogs
+
+	def self.recent
+		order("created_at DESC")
+	end
 end
