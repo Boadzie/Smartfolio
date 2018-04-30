01e56ceffccc857075a346adfc70cbf3f55ac9d9
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-04-30 16:14:36 +0000

    Implemented Markdown for blog posts

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,57 @@
 module BlogsHelper
+    def gravatar_helper user
+        image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+      end
+    
+    class CodeRayify < Redcarpet::Render::HTML
+        def block_code(code, language)
+            CodeRay.scan(code, language).div
+        end
+    end
+
+    def markdown(text)
+        coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+        options = {
+            fenced_code_blocks: true,
+            no_intra_emphasis: true,
+            autolink: true,
+            lax_html_blocks: true,
+            block_code: true,
+            block_quote: true,
+            block_html: true,
+            footnotes: true,
+            footnote_def: true,
+            header: true,
+            hrule: true,
+            list: true,
+            list_item: true,
+            paragraph: true,
+            table: true,
+            table_row: true,
+            table_cell: true,
+            codespan: true,
+            double_emphasis: true,
+            emphasis: true,
+            image: true,
+            linebreak: true,
+            link: true,
+            raw_html: true,
+            triple_emphasis: true,
+            strikethrough: true,
+            superscript: true,
+            underline: true,
+            highlight: true,
+            quote: true,
+            footnote_ref: true,
+
+        }
+
+        markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+        markdown_to_html.render(text).html_safe
+    end
+
+    def blog_status_color blog
+        'color: red;' if blog.draft?
+    end
 end
