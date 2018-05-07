9c2eac8230bcbcab24074c807946699adc86351b
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-05-07 14:03:03 +0000

    Styled blog comments with bootstrap

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,6 @@
 module BlogsHelper
     def gravatar_helper user
-        image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+        image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60, class:"img-circle"
       end
     
     class CodeRayify < Redcarpet::Render::HTML
