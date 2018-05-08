29569eca78d415dd501322fc355cd07acfeb8423
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-05-08 13:36:19 +0000

    Updated secret key base call

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Smartfolio
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
     config.load_defaults 5.1
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
