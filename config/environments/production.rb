2a45b065c20834014900c3c11a43e7d5351c648e
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-06-04 04:10:33 +0000

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_cable.allowed_request_origins = ['https://boadzie.herokuapp.com', 'http://boadzie.herokuapp.com']
+  config.action_cable.url = "wss://boadzie.herokuapp.com/cable"
 end
